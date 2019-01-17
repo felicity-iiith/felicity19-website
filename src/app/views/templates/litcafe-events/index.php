fc82f57ec3ed712d3983a6d2dc4b2558ecd83b61
Anurag Jain <anurag.jain@research.iiit.ac.in>
2019-01-18 03:23:48 +0530

    Completed all events

---
+++ src/app/views/templates/litcafe-events/index.php
@@ -0,0 +1,150 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="keywords" content="felicity, felicty18, college, fest, threads, pulsation, iiit, iiith, international, institute, information, technology, hyderabad">
+    <meta name="description" content="<?= __("Felicity is the annual technical and cultural fest of IIIT-H. Includes technical, cultural and literary events, Major nights, talks, workshops and performances. We, at IIIT-H, believe in giving back to the society and use Felicity as a medium to serve this motive and pickup various social initiatives.") ?>">
+    <meta property="og:description" content="<?= __("Felicity is the annual technical and cultural fest of IIIT-H. Includes technical, cultural and literary events, Major nights, talks, workshops and performances. We, at IIIT-H, believe in giving back to the society and use Felicity as a medium to serve this motive and pickup various social initiatives.") ?>">
+    <meta property="og:title" content="<?= isset($title) ? $title . ' · ' : '' ?><?= __('Felicity') ?> · <?= __('IIIT-H') ?>">
+    <title>Litcafe Events in Felicity 2019</title>
+<link href="https://fonts.googleapis.com/css?family=Exo" rel="stylesheet">
+<link href="/static/styles/events.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="nav-bars">
+            <a href="/threads-events">
+                <div class="nav-bar nav-bar-link">
+                    Threads
+                </div>
+            </a>
+            <a href="/litcafe-events">
+            <div class="nav-bar nav-bar-active">
+                LitCafe
+            </div>
+            </a>
+            <a href="/pulsation-events">
+            <div class="nav-bar nav-bar-link">
+                Pulsation
+            </div>
+            </a>
+            <a href="/workshops-events">
+            <div class="nav-bar nav-bar-link">
+                Workshops
+            </div> 
+            </a>
+            <a href="/culture-events">
+            <div class="nav-bar nav-bar-link">
+                Culture
+            </div>
+            </a>
+            <a href="/zombie-zone-events">
+            <div class="nav-bar nav-bar-link">
+                Zombie Zone
+            </div>
+            </a>
+            <a href="/sports-events">
+            <div class="nav-bar nav-bar-link">
+                Sports
+            </div>
+            </a>
+            <a href="/day-events">
+            <div class="nav-bar nav-bar-link">
+                Day events
+            </div>
+            </a>
+            <a href="/night-events">
+            <div class="nav-bar nav-bar-link">
+                Night events
+            </div>
+            </a>
+        </div>
+        <div class="content">
+            <div class="projector">
+                For anyone and everyone who has a creative side and enjoys literature in any form. Dive into this world of fantasies and adventures!
+            </div>
+           <div class="filmstrip">
+               <div class="boxes">
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+               </div>
+               <div class="panels">
+                    <div class="film-pic">Escape The Room,
+                        4-6PM, <br />
+                        20th January
+                    </div>                   
+                    <div class="film-pic">Treasure Hunt, <br />
+                                        5-7PM, <br />
+                                        21st January
+                    </div>                   
+                    <div class="film-pic"></div>                   
+                    <div class="film-pic"></div>                   
+               </div>              
+               <div class="boxes">
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+               </div>
+            </div> 
+        </div>
+    </div>
+    <a href="/"><img class="/static/images/avengers-logo" src="avengers-logo.png"></a>
+</body>
+</html>
---
+++ src/app/views/templates/workshop-events/index.php
@@ -0,0 +1,150 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="keywords" content="felicity, felicty18, college, fest, threads, pulsation, iiit, iiith, international, institute, information, technology, hyderabad">
+    <meta name="description" content="<?= __("Felicity is the annual technical and cultural fest of IIIT-H. Includes technical, cultural and literary events, Major nights, talks, workshops and performances. We, at IIIT-H, believe in giving back to the society and use Felicity as a medium to serve this motive and pickup various social initiatives.") ?>">
+    <meta property="og:description" content="<?= __("Felicity is the annual technical and cultural fest of IIIT-H. Includes technical, cultural and literary events, Major nights, talks, workshops and performances. We, at IIIT-H, believe in giving back to the society and use Felicity as a medium to serve this motive and pickup various social initiatives.") ?>">
+    <meta property="og:title" content="<?= isset($title) ? $title . ' · ' : '' ?><?= __('Felicity') ?> · <?= __('IIIT-H') ?>">
+    <title>Workshops in Felicity 2019</title>
+<link href="https://fonts.googleapis.com/css?family=Exo" rel="stylesheet">
+<link href="/static/styles/events.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="nav-bars">
+            <a href="/threads-events">
+                <div class="nav-bar nav-bar-link">
+                    Threads
+                </div>
+            </a>
+            <a href="/litcafe-events">
+            <div class="nav-bar nav-bar-link">
+                LitCafe
+            </div>
+            </a>
+            <a href="/pulsation-events">
+            <div class="nav-bar nav-bar-link">
+                Pulsation
+            </div>
+            </a>
+            <a href="/workshops-events">
+            <div class="nav-bar nav-bar-active">
+                Workshops
+            </div> 
+            </a>
+            <a href="/culture-events">
+            <div class="nav-bar nav-bar-link">
+                Culture
+            </div>
+            </a>
+            <a href="/zombie-zone-events">
+            <div class="nav-bar nav-bar-link">
+                Zombie Zone
+            </div>
+            </a>
+            <a href="/sports-events">
+            <div class="nav-bar nav-bar-link">
+                Sports
+            </div>
+            </a>
+            <a href="/day-events">
+            <div class="nav-bar nav-bar-link">
+                Day events
+            </div>
+            </a>
+            <a href="/night-events">
+            <div class="nav-bar nav-bar-link">
+                Night events
+            </div>
+            </a>
+        </div>
+        <div class="content">
+            <div class="projector">
+                Are you zealous to learn something? Dive into a framework or pick up a new skill.
+            </div>
+           <div class="filmstrip">
+               <div class="boxes">
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+               </div>
+               <div class="panels">
+                    <div class="film-pic">IoT Workshop<br />
+                    9.30 AM - 5 PM,<br />
+                    20-21 January
+                    </div>                   
+                    <div class="film-pic">Web and Mobile Development<br />
+                    10 AM onwards<br />
+                    20-21 January
+                    </div>                   
+                    <div class="film-pic"></div>                   
+                    <div class="film-pic"></div>                   
+               </div>              
+               <div class="boxes">
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+                    <div class="white-box"></div>
+               </div>
+            </div> 
+        </div>
+    </div>
+    <a href="/"><img class="/static/images/avengers-logo" src="avengers-logo.png"></a>
+</body>
+</html>

715e2167e369dd597434dcbbe1966b6bfd89bb18
Anurag Jain <anurag.jain@research.iiit.ac.in>
2019-01-18 03:23:24 +0530

    Added list of events

--- src/app/views/templates/schedule/index.php
+++ src/app/views/templates/schedule/index.php
@@ -2,12 +2,23 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="keywords" content="felicity, felicty18, college, fest, threads, pulsation, iiit, iiith, international, institute, information, technology, hyderabad">
+    <meta name="description" content="<?= __("Felicity is the annual technical and cultural fest of IIIT-H. Includes technical, cultural and literary events, Major nights, talks, workshops and performances. We, at IIIT-H, believe in giving back to the society and use Felicity as a medium to serve this motive and pickup various social initiatives.") ?>">
+    <meta property="og:description" content="<?= __("Felicity is the annual technical and cultural fest of IIIT-H. Includes technical, cultural and literary events, Major nights, talks, workshops and performances. We, at IIIT-H, believe in giving back to the society and use Felicity as a medium to serve this motive and pickup various social initiatives.") ?>">
+    <meta property="og:title" content="<?= isset($title) ? $title . ' · ' : '' ?><?= __('Felicity') ?> · <?= __('IIIT-H') ?>">
+    <title>Threads Events in Felicity 2019</title>
 <link href="https://fonts.googleapis.com/css?family=Exo" rel="stylesheet">
 <style>
 *{
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+}
+a {
+    text-decoration: none;
+    color: #fff;
 }
 body {
     font-family: 'Exo', sans-serif;
@@ -103,7 +114,7 @@ body {
     border-radius: 3vh;
     margin-top: 1vh;
     height: 17.5vh;
-    font-size: 3rem;
+    font-size: 1.5rem;
     justify-content: center;
     align-items:center;
     color: black;
@@ -124,33 +135,51 @@ body {
 <body>
     <div class="container">
         <div class="nav-bars">
-            <div class="nav-bar nav-bar-active">
-                Threads
-            </div>
+            <a href="/threads-events">
+                <div class="nav-bar nav-bar-active">
+                    Threads
+                </div>
+            </a>
+            <a href="/litcafe-events">
             <div class="nav-bar nav-bar-link">
                 LitCafe
             </div>
+            </a>
+            <a href="/pulsation-events">
             <div class="nav-bar nav-bar-link">
                 Pulsation
             </div>
+            </a>
+            <a href="/workshops-events">
             <div class="nav-bar nav-bar-link">
                 Workshops
             </div> 
+            </a>
+            <a href="/culture-events">
             <div class="nav-bar nav-bar-link">
                 Culture
             </div>
+            </a>
+            <a href="/zombie-zone-events">
             <div class="nav-bar nav-bar-link">
                 Zombie Zone
             </div>
+            </a>
+            <a href="/sports-events">
             <div class="nav-bar nav-bar-link">
                 Sports
-            </div>        
+            </div>
+            </a>
+            <a href="/day-events">
             <div class="nav-bar nav-bar-link">
                 Day events
             </div>
+            </a>
+            <a href="/night-events">
             <div class="nav-bar nav-bar-link">
                 Night events
-            </div> 
+            </div>
+            </a>
         </div>
         <div class="content">
             <div class="projector">

ed0293ab812113a6a73780a1086daf07cbfdfbc5
Shaunak Badani <shaunakbadani@pop-os.localdomain>
2019-01-18 17:35:40 +0530

    Added responsive sponsors page

--- src/app/views/templates/sponsors-2019/index.php
+++ src/app/views/templates/sponsors-2019/index.php
@@ -1,71 +1,123 @@
-<!DOCTYPE html>
-<html>
+    <!DOCTYPE html>
+<html lang="en">
 <head>
-<link href="https://fonts.googleapis.com/css?family=Exo" rel="stylesheet">
-<style>
-*{
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-}
-body {
-    font-family: 'Exo', sans-serif;
-    background: url('/static/images/felicity hero large.png') no-repeat;
-    background-size: cover;
-}
-.container {
-    width: 85vw;
-    height: 85vh;
-    position: absolute;
-    top: 55%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-    background-color: rgba(0,0,0,0);
-    color: white;
-}
-.content {  
-    background-color: rgba(0,0,0,0.8);
-    height: 85%;
-    padding: 3vw;
-}
-.nav-bars {
-    width: 100%;
-    height: 7.5%;
-}
-.nav-bar {
-    display: inline-block;
-    width: 40%;
-    height: 100%;
-    box-sizing: border-box;
-    clip-path: polygon(10% 0, 90% 0,100% 100%, 0 100%);
-    margin: 0 4%;
-    text-align: center;
-    padding: 2vh;
-    position: relative;
-    background-color: rgba(0,0,0,1);
-}
-.nav-bar-active{
-    color: #ccc;
-    background-color: rgba(0,0,0,0.8);
-}
-.nav-bar-link:hover{
-    color: red;
-}
-</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Exo" rel="stylesheet">
+    <title>Contact Us</title>
 </head>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Exo', sans-serif;
+    }
+    body{
+        background: url('/static/images/felicity hero large.png') no-repeat;
+        background-size: cover;
+    }
+    .sponsors{
+        /* display: flex;    */
+        font-size: 7rem;
+        text-align: center;
+    }
+    .container{
+        width: 95vw;
+        height: 85vh;
+        position: absolute;
+        top: 55%;
+        left: 50%;
+        overflow: auto;
+        transform: translate(-50%,-50%);
+        background-color: rgba(0,0,0,0.9);
+        color: white;
+        overflow: auto;
+    }
+    .coordinators{
+        height: 40%;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        margin-top: 10vh;
+        padding: 0vh 6vw;
+    }
+    .fc{
+        height: 90%;
+        width: 33%;
+        margin-bottom: 5vh;
+        vertical-align: top;
+        background-color: rgba(255,255,255,0.9);
+        box-shadow: 2vw 2vw 4vw 9vw blue;
+        clip-path: polygon(10% 0,100% 0,90% 100%,0 100%);
+    }
+    .fc .image{
+        display: flex;
+        justify-content: center;
+        height: 100%;
+        align-items: center;
+    }
+    .image img{
+        max-width: 30%;
+    }
+    .avengers {
+        position: absolute;
+        bottom: 0rem;
+        transform: scale(0.6);
+        transform-origin: bottom left;
+        left: 0rem;
+    }
+    @media(max-width:600px){
+        body{
+            background: url('/static/images/felicity-hero-large-mobile.png') no-repeat;
+            background-size: cover;
+        }
+        .coordinators{
+            height: 100%;
+            width: 90%;
+        }
+        .fc{
+            display: inline-block;
+            width: 100%;
+            height: 31%;
+            margin: 2vh 0;
+        }
+        .coordinators{
+            flex-direction: column;
+        }
+    }
+</style>
 <body>
     <div class="container">
-        <div class="nav-bars">
-            <div class="nav-bar nav-bar-active">
-                About Us
-            </div>
-            <div class="nav-bar nav-bar-link">
-                Team
-            </div>  
+        <div class="sponsors">
+           Our Sponsors
         </div>
-        <div class="content">
-        <?= __($about_us) ?>
+        <div class="coordinators">
+            <div class="fc">
+                <div class="image" style="transform: scale(2)">
+                    <img src="/static/images/sponsors/19/SBI.svg" />
+                </div>
+            </div>
+            <div class="fc">
+                <div class="image" style="transform: scale(2)">
+                    <img src="/static/images/sponsors/19/zebronics.png" />
+                </div>
+            </div>
+            <div class="fc">
+                <div class="image">
+                    <img src="/static/images/sponsors/19/airtel.svg" />
+                </div>
+            </div>
+            <div class="fc" style="background-color: rgb(35,31,32)">
+                <div class="image" style="transform: scale(2)">
+                    <img src="/static/images/sponsors/19/Ptron.png" />
+                </div>
+            </div>
+        </div>
+        <div class="avengers">
+            <a href="/"><img src="/static/images/avengers-logo.png" /></a>
         </div>
     </div>
-</body> 
+</body>
 </html>
